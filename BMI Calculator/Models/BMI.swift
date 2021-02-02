7b26c34b8b0174306f32e10030a09db6e7bd2dd8
Ahmed Nagy <ahmed.nagy.radwan@gmail.com>
2021-02-02 09:31:32 +0200

    end

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,18 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Nagy on 02/02/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+
+import UIKit
+
+struct BMI {
+    
+    let value : Float?
+    let advice : String?
+    let color  : UIColor
+    
+}
